4e67ce9391b225b61dc6253d3658ee150571e44c
asherjames <asherjames.nz@gmail.com>
2018-05-31 20:55:30 +1200

    Add nzff base url config

--- src/main/kotlin/ash/java/nzfforganiser/dao/NzffDao.kt
+++ src/main/kotlin/ash/java/nzfforganiser/dao/NzffDao.kt
@@ -1,6 +1,7 @@
 package ash.java.nzfforganiser.dao
 
 import ash.java.nzfforganiser.model.Movie
+import org.springframework.beans.factory.annotation.Value
 import org.springframework.stereotype.Service
 
 interface NzffDao
@@ -11,6 +12,9 @@ interface NzffDao
 @Service
 class NzffDaoImpl : NzffDao
 {
+  @Value("\${nzff.baseUrl}")
+  private lateinit var nzffBaseUrl: String
+
   override fun retrieveWishlist(id: String): List<Movie>
   {
     TODO("not implemented")
--- src/main/kotlin/ash/java/nzfforganiser/resource/NzffOrgResource.kt
+++ src/main/kotlin/ash/java/nzfforganiser/resource/NzffOrgResource.kt
@@ -3,16 +3,24 @@ package ash.java.nzfforganiser.resource
 import ash.java.nzfforganiser.dao.NzffDao
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.stereotype.Component
+import javax.inject.Singleton
 import javax.ws.rs.Path
 import javax.ws.rs.QueryParam
 import javax.ws.rs.core.Response
 
 @Component
+@Singleton
 @Path("/nzfforg")
 class NzffOrgResource @Autowired constructor(private val nzffDao: NzffDao)
 {
   @Path("/wishlist")
   fun getOrganisedWishlist(@QueryParam("id") id: String) : Response
+  {
+    return Response.ok().build()
+  }
+
+  @Path("/ping")
+  fun pong() : Response
   {
     return Response.ok().build()
   }
