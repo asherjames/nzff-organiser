6629e0880a40228c04f9d08bb7a8e2a1b8fb1c45
asherjames <asherjames.nz@gmail.com>
2018-06-20 22:31:49 +1200

    Add exception to handle case when no suitable schedule is found

--- src/main/kotlin/ash/java/nzfforganiser/ExceptionAdvisor.kt
+++ src/main/kotlin/ash/java/nzfforganiser/ExceptionAdvisor.kt
@@ -1,5 +1,6 @@
 package ash.java.nzfforganiser
 
+import ash.java.nzfforganiser.exception.NoAcceptableScheduleFoundException
 import ash.java.nzfforganiser.exception.ScraperException
 import ash.java.nzfforganiser.exception.TooManySchedulesException
 import ash.java.nzfforganiser.model.NzffResponse
@@ -29,6 +30,16 @@ class ExceptionAdvisor : ResponseEntityExceptionHandler()
         .status(HttpStatus.BAD_REQUEST)
         .body(NzffResponse(
             message = "There are too many scheduling options, please select less films"
+        ))
+  }
+
+  @ExceptionHandler(NoAcceptableScheduleFoundException::class)
+  fun handleNoAcceptableScheduleFound(e: NoAcceptableScheduleFoundException): ResponseEntity<NzffResponse>
+  {
+    return ResponseEntity
+        .status(HttpStatus.NOT_FOUND)
+        .body(NzffResponse(
+            message = "Could not find a suitable session schedule"
         ))
   }
 
--- src/main/kotlin/ash/java/nzfforganiser/exception/Exceptions.kt
+++ src/main/kotlin/ash/java/nzfforganiser/exception/Exceptions.kt
@@ -2,6 +2,7 @@ package ash.java.nzfforganiser.exception
 
 class ScraperException : Exception
 {
+  constructor() : super()
   constructor(message: String) : super(message)
   constructor(cause: Throwable) : super(cause)
   constructor(message: String, cause: Throwable) : super(message, cause)
@@ -9,6 +10,15 @@ class ScraperException : Exception
 
 class TooManySchedulesException : Exception
 {
+  constructor() : super()
+  constructor(message: String) : super(message)
+  constructor(cause: Throwable) : super(cause)
+  constructor(message: String, cause: Throwable) : super(message, cause)
+}
+
+class NoAcceptableScheduleFoundException : Exception
+{
+  constructor() : super()
   constructor(message: String) : super(message)
   constructor(cause: Throwable) : super(cause)
   constructor(message: String, cause: Throwable) : super(message, cause)
